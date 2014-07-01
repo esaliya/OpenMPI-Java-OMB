db8de3465eee9fc756f544b4b6709b2700d14bb1
Saliya Ekanayake <sekanaya@cs.indiana.edu>
2014-06-30 23:00:15 -0400

    initial commit with two benchmarks

---
+++ src/main/java/org/saliya/omb/OsuLaten.java
@@ -0,0 +1,95 @@
+package org.saliya.omb;
+
+import mpi.Intracomm;
+import mpi.MPI;
+import mpi.MPIException;
+import org.saliya.util.MpiOps;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.nio.ByteBuffer;
+
+public class OsuLaten {
+    public static void main(String[] args) throws MPIException {
+        MPI.Init(args);
+
+        Intracomm comm = MPI.COMM_WORLD;
+        int rank = comm.getRank();
+        int numProcs = comm.getSize();
+
+        if (numProcs != 2){
+            System.out.println("This test requires exactly two processes\n");
+            MPI.Finalize();
+            return;
+        }
+
+        int maxMsgSize = 1<<10; // 1KB, i.e. 1024 bytes
+        int largeMsgSize = 8192;
+        int skip = 1000;
+        int skipLarge = 10;
+        int iterations = 10000;
+        int iterationsLarge = 100;
+
+        if (args.length >= 1){
+            maxMsgSize = Integer.parseInt(args[0]);
+        }
+
+        if (args.length == 2){
+            iterations = iterationsLarge = Integer.parseInt(args[1]);
+        }
+
+        int byteBytes = maxMsgSize;
+        ByteBuffer sbuff = MPI.newByteBuffer(byteBytes);
+        ByteBuffer rbuff = MPI.newByteBuffer(byteBytes);
+
+
+
+        String msg = "Rank " + rank + " is on " + MPI.getProcessorName() + "\n";
+        msg = MpiOps.allReduceStr(msg, comm);
+        if (rank == 0){
+            System.out.println(msg);
+            System.out.println("#Bytes\tLatency(us)");
+            System.out.println("Max message size in chars (casted to bytes): " + maxMsgSize + " in bytes:" + maxMsgSize);
+        }
+
+        for (int numBytes = 0; numBytes <= maxMsgSize; numBytes = (numBytes == 0 ? 1 : numBytes*2)){
+            /* initialize buffers for each run*/
+            for (int i = 0; i < byteBytes; ++i){
+                sbuff.put(i,(byte)'a');
+                rbuff.put(i,(byte)'b');
+            }
+
+            if (numBytes > largeMsgSize){
+                skip = skipLarge;
+                iterations = iterationsLarge;
+            }
+            comm.barrier();
+
+            double tStart = 0.0, tStop = 0.0;
+            double minLatency, maxLatency, avgLatency;
+
+            if (rank == 0){
+                for (int i = 0; i < iterations + skip; ++i){
+                    if (i == skip){
+                        tStart = MPI.wtime();
+                    }
+                    comm.send(sbuff,numBytes,MPI.BYTE,1,1);
+                    comm.recv(rbuff,numBytes,MPI.BYTE,1,1);
+                }
+                tStop = MPI.wtime();
+            } else if (rank == 1){
+                for (int i = 0; i < iterations + skip; ++i){
+                    comm.recv(rbuff,numBytes,MPI.BYTE,0,1);
+                    comm.send(sbuff,numBytes,MPI.BYTE,0,1);
+                }
+            }
+
+
+            if (rank == 0){
+                double latency = (tStop - tStart)*1e6 /(2.0 * iterations);
+                System.out.println(numBytes + "\t" + new BigDecimal(latency).setScale(2, RoundingMode.UP).doubleValue());
+            }
+        }
+        MPI.Finalize();
+    }
+}
---
+++ src/main/java/org/saliya/util/MpiOps.java
@@ -0,0 +1,40 @@
+package org.saliya.util;
+
+import mpi.Intracomm;
+import mpi.MPI;
+import mpi.MPIException;
+
+
+public class MpiOps {
+    public static String allReduceStr(String value, Intracomm comm) throws MPIException {
+        int size = comm.getSize();
+        int [] lengths = new int[size];
+        int length = value.length();
+        int rank = comm.getRank();
+        lengths[rank] = length;
+        comm.allGather(lengths, 1, MPI.INT);
+        int [] displas = new int[size];
+        displas[0] = 0;
+        System.arraycopy(lengths, 0, displas, 1, size - 1);
+        if (displas.length > 2){
+            for (int i = 2; i < displas.length; ++i){
+                displas[i] += displas[i-1];
+            }
+        }
+
+        /*Arrays.parallelPrefix(displas, new IntBinaryOperator() {
+            @Override
+            public int applyAsInt(int m, int n) {
+                return m + n;
+            }
+        });*/
+        int count = 0;
+        for (int i=0; i < lengths.length; ++i){
+            count += lengths[i];
+        }
+        char [] recv = new char[count];
+        System.arraycopy(value.toCharArray(), 0,recv, displas[rank], length);
+        comm.allGatherv(recv, lengths, displas, MPI.CHAR);
+        return  new String(recv);
+    }
+}
