cc8b75e652ed8a59c39c304592c51935b277195f
twilbon <toriwilbon@gmail.com>
2014-07-07 18:00:54 -0400

    Adding OsuAllGather

---
+++ src/main/java/org/saliya/omb/collectives/OsuAllGather.java
@@ -0,0 +1,10 @@
+package org.saliya.omb.collectives;
+
+/**
+ * Created by Tori on 7/7/2014.
+ */
+public class OsuAllGather {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
