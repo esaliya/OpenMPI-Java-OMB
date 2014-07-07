86fda6802632a12aed3c775bce744cfc5b767d4c
Nigel Pugh <nigel.pugh32@gmail.com>
2014-07-07 17:28:28 -0400

    Adding OsuBroadcast

---
+++ src/main/java/org/saliya/omb/collectives/OsuBroadcast.java
@@ -0,0 +1,10 @@
+package org.saliya.omb.collectives;
+
+/**
+ * Created by nigelp32 on 7/7/14.
+ */
+public class OsuBroadcast {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
