a94ca6021b94b331910c23b6be093e63d5552900
Saliya Ekanayake <esaliya@gmail.com>
2016-06-04 12:00:05 -0400

    adding debugs to allgather

--- src/main/java/org/saliya/ompi/omb/ParallelOps.java
+++ src/main/java/org/saliya/ompi/omb/ParallelOps.java
@@ -389,7 +389,7 @@ public class ParallelOps {
             mmapCollectiveBytes.writeByte(offset+i, sbuff.get(i));
         }
         // TODO - debugs
-        System.out.println("Rank: " + worldProcRank + " " + (char)mmapCollectiveBytes.readByte(0));
+        System.out.println("--Rank: " + worldProcRank + " " + (char)mmapCollectiveBytes.readByte(0));
         worldProcsComm.barrier();
         if(isMmapLead){
             cgProcComm.allGather(mmapCollectiveByteBuffer, numBytes*mmapProcsCount, MPI.BYTE, mmapCollectiveByteBuffer2, numBytes*mmapProcsCount, MPI.BYTE);
